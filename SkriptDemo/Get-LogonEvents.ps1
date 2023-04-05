8677d58511c49a0ceb2bb075fca15bd0061e1a6b
Stefan Ober <StefanO@ppedv.de>
2023-04-05 15:03:26 +0200

    Skript Beispiel mit Parameter

---
+++ SkriptDemo/Get-LogonEvents.ps1
@@ -0,0 +1,14 @@
+﻿[cmdletBinding()]
+param(
+[Parameter(Mandatory=$true)]
+[int]$EventId ,
+
+[int]$Newest = 5 ,
+
+[string]$Computername = "localhost"
+)
+
+
+Get-EventLog -LogName Security -ComputerName $Computername | Where-Object EventId -eq $EventId | Select-Object -First $Newest
+
+
