4e6eed64cafc81a651111318804d4538695b0b54
Stefan Ober <StefanO@ppedv.de>
2023-04-06 15:04:41 +0200

    Skript zur Auswertung

---
+++ Hilfreiches/Get-GroupMemberwithLogonTime.ps1
@@ -0,0 +1,14 @@
+﻿[cmdletBinding()]
+param(
+[Parameter(Mandatory=$true)]
+[string]$Groupname
+)
+
+$Groupmember = Get-ADGroupMember -Identity $Groupname
+foreach($User in $Groupmember)
+{
+  $Users =  Get-ADUser -Identity $PSItem.SamAccountName -Properties LastLogonTimestamp 
+}
+
+Format-Table -InputObject $User -Property Name,@{n="LastLogon";e={[datetime]::FromFileTime($PSItem.LastLogonTimestamp)}}
+
