68989a15866e6e082d61389230ad51cab3f43a05
Stefan Ober <StefanO@ppedv.de>
2023-04-06 10:43:34 +0200

    Functions und SkriptModule

---
+++ SkriptDemo/MoeglicheLoesung-Function.ps1
@@ -0,0 +1,92 @@
+﻿[cmdletBinding()]
+param(
+
+[Parameter(Mandatory=$true)]
+[ValidateScript({Test-Path -Path $PSItem -PathType Container})]
+[string]$Path, 
+
+[ValidateRange(1,100)]
+[int]$DirCount = 3,
+
+[ValidateRange(1,100)]
+[int]$FileCount = 9,
+
+[ValidateLength(3,15)]
+[string]$Name = "TestFiles",
+
+[switch]$Force
+)
+## FunktionsDeklaration
+function New-TestFiles
+{
+[cmdletBinding()]
+param(
+[Parameter(Mandatory=$true)]
+[ValidateScript({Test-Path -Path $PSItem -PathType Container})]
+[string]$Path,
+
+[ValidateRange(1,100)]
+[int]$FileCount = 9,
+
+[ValidateLength(3,15)]
+[string]$BaseName = "Datei"
+)
+
+for($i = 1; $i -le $FileCount; $i++)
+{
+    $Filenumber = "{0:D3}" -f $i
+    $FileName = "$BaseName$Filenumber.txt"
+    New-Item -Path $Path -Name $FileName -ItemType File
+}
+
+}
+##
+if($Path.EndsWith("\") -ne $true)
+{
+    $Path += "\"
+}
+
+if(Test-Path -Path ($Path + $Name) -PathType Container)
+{
+    if($Force)
+    {
+        Remove-Item -Path ($Path + $Name) -Recurse -Force
+    }
+    else
+    {
+        Write-Host -ForegroundColor Red -Object "Ordner bereits vorhanden"
+        exit
+    }
+}
+
+
+$TestFileDir = New-Item -Path $Path -Name $Name -ItemType Directory
+
+#Dateien im Root Verzeichnis
+<# Ersetzt durch Funktion 
+for($i = 1; $i -le $FileCount; $i++)
+{
+    $Filenumber = "{0:D3}" -f $i
+    $FileName = "File$Filenumber.txt"
+    New-Item -Path $TestFileDir.FullName -Name $FileName -ItemType File
+}
+#>
+New-TestFiles -Path $TestFileDir.FullName -FileCount $FileCount
+
+for($i = 1; $i -le $DirCount; $i++)
+{
+    $DirNumber = "{0:D3}" -f $i
+    $DirName = "Directory$DirNumber"
+    $subdir = New-Item -Path $TestFileDir.FullName -Name $DirName -ItemType Directory
+
+    Write-Debug -Message "Schleife - vor Dateierstellung in Ordnererstellung"
+
+    <#
+    for($j = 1; $j -le $FileCount; $j ++)
+    {        
+        $Filenumber = "{0:D3}" -f $j
+        $FileName = "Dir$($Dirnumber)_File$Filenumber.txt"
+        New-Item -Path $subdir.FullName -Name $FileName -ItemType File
+    }#>
+    New-TestFiles -Path $subdir.FullName -FileCount $FileCount -BaseName "Dir$($DirNumber)_Datei"
+}
